ebdeae43b4933e6c38a0ea67e2b448c9594a87b0
Eryk Nejfert <110306022+ErykNe@users.noreply.github.com>
2024-01-22 22:15:18 +0100

    Additions
    Chess board added that s lit🔥

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
-import './ChessBoard.css'
+import './ChessBoard.css';
+
+const verticalAxis = ["1","2","3","4","5","6","7","8"]
+const horizontalAxis = ["a","b","c","d","e","f","g","h"]
 
 export default function ChessBoard(){
-    return <div>test</div>
+    let board : React.JSX.Element [] = ChessGrids()
+    return <div id="chessBoard">{board}</div>
+}
+export function ChessGrids(){
+    console.log("ChessGrids function called");
+    let board : React.JSX.Element [] = []
+    for(let j = verticalAxis.length - 1; j >= 0; j--) {
+        for(let i = 0; i < horizontalAxis.length; i++){
+            board.push(
+                <div id="pushedGrids" style={{ backgroundColor: i%2!=0 && j%2==0 || i%2==0 && j%2!=0 ? 'white' : 'initial' }}>
+                    [{horizontalAxis[i]}{verticalAxis[j]}]
+                </div>
+            )
+        }
+    }
+    return board;
 }
