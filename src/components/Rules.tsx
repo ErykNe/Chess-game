15c680f985b006e99bf9993538f39fa1811ddb21
Eryk Nejfert <110306022+ErykNe@users.noreply.github.com>
2024-02-07 21:16:47 +0100

    Rules class prototype added

---
+++ src/components/Rules.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ChessBoard from "./ChessBoard";
+import { board } from "./ChessBoard.tsx";
+
+class Rules {
+
+}
+
+class EnPassant {
+    
+}
+
+class PawnPromoting {
+    
+}
+
+class Check {
+    
+}
+
+class KingLegalMoves {
+    
+}
+
+class PieceFork {
+    
+}
+
+class Checkmate {
+    
+}
